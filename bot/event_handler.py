32b62bcddb702d9a142402bc6cb097f99cce674c
Nick Comment <ncomment@Nick-Comment.local>
2016-08-09 22:56:56 -0400

    testing

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -29,8 +29,6 @@ class RtmEventHandler(object):
         elif event_type == 'group_joined':
             # you joined a private group
             self.msg_writer.write_help_message(event['channel'])
-        elif event_type == 'command':
-            self.render_latex(event['channel'],'asdf')
         else:
             pass
 
@@ -40,15 +38,5 @@ class RtmEventHandler(object):
 
             msg_txt = event['text']
             
-            if self.clients.is_bot_mention(msg_txt):
-                # e.g. user typed: "@pybot tell me a joke!"
-                if 'help' in msg_txt:
-                    self.msg_writer.write_help_message(event['channel'])
-                elif re.search('hi|hey|hello|howdy', msg_txt):
-                    self.msg_writer.write_greeting(event['channel'], event['user'])
-                elif 'joke' in msg_txt:
-                    self.msg_writer.write_joke(event['channel'])
-                elif 'attachment' in msg_txt:
-                    self.msg_writer.demo_attachment(event['channel'])
-                else:
-                    self.msg_writer.write_prompt(event['channel'])
+            if event['text'][:5] == 'latex':
+                self.msg_writer.render_latex(event['channel'])
